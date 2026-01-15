00885706608e02e9009eed3da87cad291f3af85c
zhelepas <pasko.zhelev@diconium.com>
2026-01-15 11:37:27 +0100

    added new projects-data.json

--- src/GlobalStore.js
+++ src/GlobalStore.js
@@ -1,81 +1,35 @@
 import React, { useState } from "react";
+import rawData from "./projects-data.json";
 
-// starting date is the first day of the current year
-// ignoring the date time
-const startingDate = new Date(2026, 0, 1);
+const formattedInitialState = () => {
+    const { startingDate, teams } = rawData;
+    // Create the date object once
+    const dateObj = new Date(startingDate);
 
-// name pairs generated with https://commentpicker.com/combination-generator.php
+    const processedTeams = {};
+    
+    Object.keys(teams).forEach((key) => {
+        processedTeams[key] = {
+            ...teams[key],
+            startDate: dateObj, // Injects the actual Date object
+            people: teams[key].people || [],
+            dutyList: teams[key].dutyList || []
+        };
+    });
 
-const initialState = {
-    "dsv-redesign":
-    { 
-        name: "DSV Redesign", 
-        startDate: startingDate,
-        people: [
-            "Sandra",
-            "Marta",
-            "Joao",
-            "Sara",
-            "Benjamin",
-            "Roland",
-            "Daniel"
-        ]
-    },  
-    "mfh-dev":
-    { 
-            name: "MFH Dev Team", 
-            startDate: startingDate,
-            people: [
-                "Arthur",
-                "Bruno S.",
-                "Marnie",
-                "Stefan",
-                "João",
-                "Margarida",
-                "Tiago",
-                "Bruno M. F."
-            ]
-    },
-    "ces-team": {
-        name: "CES Dev Team", 
-        startDate: startingDate,
-        people: [
-            "Torsten",
-            "Vijay",
-            "Garima",
-            "Mariana",
-            "Payal",
-            "Sravani",
-            "Filipa",
-            "Praveen",
-            "Navneet"
-        ]
-    },
-    "erwin": {
-        name: "erWin Team", 
-        startDate: startingDate,
-        people: [
-            "Tiago",
-            "Ricardo",
-            "Andrei",
-            "Felipe",
-            "Dilara",
-            "Vanessa",
-            "Chaitra",
-            "Gnana",
-            "Philipp"
-        ]
-    }
-}
+    return processedTeams;
+};
 
 export const Context = React.createContext();
 
 const GlobalStore = ({ children }) => {
-    const [globalState, setGlobalState] = useState(initialState);
+    const [globalState, setGlobalState] = useState(formattedInitialState());
 
     return (
-        <Context.Provider value={[globalState, setGlobalState]}>{children}</Context.Provider>
+        <Context.Provider value={[globalState, setGlobalState]}>
+            {children}
+        </Context.Provider>
     );
-}
+};
 
 export default GlobalStore;
